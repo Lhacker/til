598499119055bab594c130c0271aac8b43fdb81c
Lhacker <starclown1@gmail.com>
2016-03-19 18:04:50 +0900

    add generator

--- elixir/samples/for.ex
+++ elixir/samples/for.ex
@@ -2,3 +2,4 @@ IO.inspect for x <- 1..10, x * x < 40, do: x * x
 IO.inspect (for x <- 1..10, x * x < 40 do
   x * x
 end)
+IO.inspect for i <- [:a, :b, :c], j <- [1, 2], do: [i, j]
---
+++ elixir/samples/for_into.ex
@@ -0,0 +1,2 @@
+IO.inspect for <<c <- " hello, world ">>, c != ?\s, into: "hoge", do: <<c>>
+IO.inspect for x <- 1..100, rem(x, 2) == 0, into: [9999, 9999], do: x, limit: :infinity
