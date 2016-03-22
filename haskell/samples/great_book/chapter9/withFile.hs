c100d0780904d7d73eaf34ae5a2436981eab9548
Lhacker <starclown1@gmail.com>
2016-03-22 21:42:00 +0900

    add files

---
+++ haskell/samples/great_book/chapter9/withFile.hs
@@ -0,0 +1,6 @@
+import Control.Exception
+
+withFile :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile name mode f = bracket (openFile name mode)
+    (\handle -> hClose handle)
+    (\handle -> f handle)
