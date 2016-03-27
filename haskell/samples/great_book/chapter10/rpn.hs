d30c3e872e768ab83087ee1e5eed1a719e873df3
Lhacker <starclown1@gmail.com>
2016-03-27 22:54:40 +0900

    add RPN(reverse polish notation) file

---
+++ haskell/samples/great_book/chapter10/rpn.hs
@@ -0,0 +1,11 @@
+solveRPN :: String -> Double
+solveRPN = head . foldl foldingFunction [] . words
+  where
+    foldingFunction (x:y:xs) "*" = (y * x):xs
+    foldingFunction (x:y:xs) "+" = (y + x):xs
+    foldingFunction (x:y:xs) "-" = (y - x):xs
+    foldingFunction (x:y:xs) "/" = (y / x):xs
+    foldingFunction (x:y:xs) "^" = (y ** x):xs
+    foldingFunction (x:xs) "ln" = (log x):xs
+    foldingFunction xs "sum" = [sum xs]
+    foldingFunction xs numberString = read numberString:xs
