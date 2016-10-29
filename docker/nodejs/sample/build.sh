9bb5bc64ef3efaa6d1889617270e960a4e095169
Lhacker <starclown1@gmail.com>
2016-10-29 15:58:06 +0900

    improve docker file(consider layer)

--- docker/nodejs/sample/build.sh
+++ docker/nodejs/sample/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build --force-rm --no-cache --build-arg NODEJS_VERSION=v7.0.0 -t centos:7_nodejs_v7.0.0 .
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+docker build --force-rm --no-cache --build-arg NODEJS_VERSION=v7.0.0 -t centos:7_nodejs_v7.0.0 $SCRIPT_DIR
