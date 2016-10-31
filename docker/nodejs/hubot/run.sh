7cba678c70abab08066d3379de0757758af9bd11
Lhacker <starclown1@gmail.com>
2016-10-31 22:39:13 +0900

    modify hubot/run.sh

--- docker/nodejs/hubot/run.sh
+++ docker/nodejs/hubot/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-VOL_DIR=${1:-$SCRIPT_DIR/scripts}
+HUBOT_SLACK_TOKEN=$1
+VOL_DIR=$SCRIPT_DIR/scripts
 HUBOT_DIR=/myhubot
-docker run -it --rm --name hubot -v $VOL_DIR:$HUBOT_DIR/scripts -w $HUBOT_DIR centos:7_hubot /bin/bash
+
+docker run -it --rm --name hubot \
+  -v $VOL_DIR:$HUBOT_DIR/scripts \
+  -e HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN \
+  -w $HUBOT_DIR \
+  centos:7_hubot \
+  /bin/bash
