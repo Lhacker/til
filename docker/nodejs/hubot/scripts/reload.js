50ee742d675f4fef82c384987cda6a72a1649fc8
Lhacker <starclown1@gmail.com>
2016-11-15 03:11:41 +0900

    refactor reload.js

--- docker/nodejs/hubot/scripts/reload.js
+++ docker/nodejs/hubot/scripts/reload.js
@@ -1,3 +1,4 @@
+// Ref: https://github.com/github/hubot-scripts/blob/master/src/scripts/reload.coffee
 (function(module) {
   const fs = require('fs');
   const path = require('path');
@@ -9,20 +10,15 @@
     robot.hear(/command count/i, function(res) {
       res.send(`I am aware of ${res.robot.commands.length} + commands`);
     });
-    robot.hear(/commands data/i, function(res) {
-      res.send(JSON.parse(res.robot.commands));
-    });
 
     robot.respond(/reload scripts/i, function(res) {
       try {
-        oldCommands = robot.commands;
-        oldListeners = robot.listeners;
-        robot.commands = [];
-        robot.listeners = [];
+        oldCommands = res.robot.commands;
+        oldListeners = res.robot.listeners;
+        res.robot.commands = [];
+        res.robot.listeners = [];
 
-        reloadAllScripts(res, success, function(err) {
-          res.send(err);
-        });
+        reloadAllScripts(res, successReloading, errorReloading);
       } catch (error) {
         console.log(`Hubot reloader ${error}`);
         res.send(`Could not reload all scripts: ${error}`);
@@ -30,63 +26,85 @@
     });
   };
 
-  reloadAllScripts = function(res, success, error) {
-    var robot = res.robot;
-    robot.emit('reload_scripts');
-
-    var scriptsPath = Path.resolve(".", "scripts");
-    robot.load(scriptsPath);
-
-    var srcScriptsPath = Path.resolve(".", "src", "scripts");
-    robot.load(scriptsPath);
-
-    var hubotScripts = Path.resolve(".", "hubot-scripts.json");
-    robot.load(scriptsPath);
-    fs.exists(hubotScripts, function(exists) {
-        if (exists) {
-          fs.readFile(hubotScripts, function(err, data) {
-            if (data.length > 0) {
-              try {
-                var scripts = JSON.parse(data);
-                var nodeModulesScriptsPath = Path.resolve("node_modules", "hubot-scripts", "src", "scripts");
-                robot.loadHubotScripts(nodeModulesScriptsPath);
-              } catch (err) {
-                error(`Error parsing JSON data from hubot-scripts.json: ${err}`);
-                return;
-              }
-            }
-          });
-        }
-    });
-
-    var externalScripts = Path.resolve(".", "external-scripts.json");
-    fs.exists(externalScripts, function(exists) {
-      if (exists) {
-        fs.readFile(externalScripts, function(err, data) {
-          if (data.length > 0) {
-            var scripts = null;
-            try {
-              scripts = JSON.parse(data);
-            } catch (err) {
-              error(`Error parsing JSON data from hubot-scripts.json: ${err}`);
-              return;
-            }
-            robot.loadExternalScripts(scripts);
-            return;
-          }
-        });
-      }
-    });
-    afterReloaded(res);
-  };
-
-  var afterReloaded = function(res) {
+  var successReloading = function(res) {
     // cleanup old listeners and help
     for (var listener in oldListeners) {
       listener = {};
     }
     oldListeners = null;
     oldCommands = null;
+    res.send("Reloaded all scripts!");
+  };
+
+  var errorReloading = function(res, errorMessage) {
+    res.send(errorMessage);
+  };
+
+  var reloadAllScripts = function(res, successReloading, errorReloading) {
+    var robot = res.robot;
+    robot.emit('reload_scripts');
+
+    // reload scripts
+    robot.load(path.resolve(".", "scripts"));
+    robot.load(path.resolve(".", "src", "scripts"));
+    if (!reloadHubotScripts(res, errorReloading)) {
+      return;
+    }
+    if (!reloadExternalScripts(res, errorReloading)) {
+      return;
+    }
+
+    successReloading(res);
+  };
+
+  var reloadHubotScripts = function(res, errorReloading) {
+    var robot = res.robot;
+    var hubotScripts = path.resolve(".", "hubot-scripts.json");
+    fs.access(hubotScripts, function(accessError) {
+      if (accessError) {
+        errorReloading(`Cannot access ${hubotScripts}`);
+        return false;
+      } else {
+        fs.readFile(hubotScripts, function(readError, data) {
+          if (data.length > 0) {
+            try {
+              var scripts = JSON.parse(data);
+              var nodeModulesScriptsPath = path.resolve("node_modules", "hubot-scripts", "src", "scripts");
+              res.robot.loadHubotScripts(nodeModulesScriptsPath, scripts);
+            } catch (err) {
+              errorReloading(res, `Error parsing JSON data from hubot-scripts.json: ${err}`);
+              return false;
+            }
+          }
+        });
+      }
+    });
+    return true;
+  };
+
+  var reloadExternalScripts = function(res, errorReloading) {
+    var robot = res.robot;
+    var externalScripts = path.resolve(".", "external-scripts.json");
+    fs.access(externalScripts, function(accessError) {
+      if (accessError) {
+        errorReloading(`Cannot access ${externalScripts}`);
+        return false;
+      } else {
+        fs.readFile(externalScripts, function(readError, data) {
+          if (data.length > 0) {
+            var scripts = null;
+            try {
+              scripts = JSON.parse(data);
+            } catch (err) {
+              errorReloading(`Error parsing JSON data from external-scripts.json: ${err}`);
+              return false;
+            }
+            robot.loadExternalScripts(scripts);
+          }
+        });
+      }
+    });
+    return true;
   };
 
 })(module);
