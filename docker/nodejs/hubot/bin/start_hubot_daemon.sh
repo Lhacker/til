389859d13cf4d6d89f149fc2dc317b810ed0e700
Lhacker <starclown1@gmail.com>
2016-11-22 00:45:50 +0900

    modify hubot docker
    use 'pm2' for automatically restarting and watching script change

---
+++ docker/nodejs/hubot/bin/start_hubot_daemon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+LOG_DIR=/var/log/hubot
+
+pm2 -l $LOG_DIR/out.log --watch $SCRIPT_DIR/scripts start $SCRIPT_DIR/run_hubot.sh
+tail -f /dev/null
--- docker/nodejs/hubot/run.sh
+++ docker/nodejs/hubot/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 function usage() {
@@ -7,15 +8,29 @@ function usage() {
   exit 1
 }
 
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  usage
+fi
+
 HUBOT_SLACK_TOKEN=$1
 HUBOT_SLACK_PORT=${2:-9000}
-VOL_DIR=$SCRIPT_DIR/scripts
 HUBOT_DIR=/myhubot
+HUBOT_SCRIPT_DIR=${HUBOT_DIR}/scripts
+HUBOT_DAEMON_PATH=$HUBOT_DIR/start_hubot_daemon.sh
+HUBOT_LOG_DIR=/var/log/hubot
 
-docker run -it --rm --name hubot \
+HOST_SLACK_PORT=$HUBOT_SLACK_PORT
+HOST_SCRIPT_DIR=$SCRIPT_DIR/scripts
+HOST_LOG_DIR=$SCRIPT_DIR/log
+
+docker run -itd \
+  --name hubot \
+  --hostname hubot \
   -e HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN \
   -e PORT=$HUBOT_SLACK_PORT \
-  -v $VOL_DIR:$HUBOT_DIR/scripts \
+  -p ${HOST_SLACK_PORT}:${HUBOT_SLACK_PORT} \
+  -v ${HOST_SCRIPT_DIR}:${HUBOT_SCRIPT_DIR} \
+  -v ${HOST_LOG_DIR}:${HUBOT_LOG_DIR} \
   -w $HUBOT_DIR \
   centos:7_hubot \
-  /bin/bash
+  bash -c "source /etc/profile; $HUBOT_DAEMON_PATH"
