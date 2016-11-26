c425056655e0488e2bf01048512a2df11454ecab
Lhacker <starclown1@gmail.com>
2016-11-27 00:29:51 +0900

    update a little

--- docker/memcached/sample/run.sh
+++ docker/memcached/sample/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-name="memcached"
-image="centos:7_memcached"
+name=memcached
+image=centos:7_memcached
 
-docker run -d --privileged --name $name $image /sbin/init
-docker exec -it $name /bin/bash
+docker run -d --privileged --name ${name} ${image} /sbin/init
+docker exec -it ${name} /bin/bash
