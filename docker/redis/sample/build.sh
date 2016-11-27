1318c196bd22113cc69cf6bea3ccab965dc44610
Lhacker <starclown1@gmail.com>
2016-11-27 13:17:13 +0900

    Modify redis install flow and add redis.conf

--- docker/redis/sample/build.sh
+++ docker/redis/sample/build.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 REDIS_VERSION=3.2.5
 TAG=centos:7_redis_${REDIS_VERSION}
-docker build --force-rm --no-cache -t ${TAG} ${SCRIPT_DIR}
+docker build --force-rm --no-cache --build-arg REDIS_VERSION=3.2.5 -t ${TAG} ${SCRIPT_DIR}
