490ae89febf67bf3db80c737c76a040bbd83e6cb
Lhacker <starclown1@gmail.com>
2016-12-08 01:14:03 +0900

    add https://github.com/lmarkus/hubot-conversation

--- docker/hubot/sample/build.sh
+++ docker/hubot/sample/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-docker build --force-rm --no-cache -t centos:7_hubot $SCRIPT_DIR
+docker build --force-rm --build-arg HUBOT_CONVERSATION_PLUGIN=true --no-cache -t centos:7_hubot $SCRIPT_DIR
