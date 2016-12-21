ba1812874510cd1a0472757f84016c498568ec31
Lhacker <starclown1@gmail.com>
2016-12-22 01:02:51 +0900

    change directory construction and let data and access log remain

--- docker/jenkins/jenkins1_651_3/run.sh
+++ docker/jenkins/jenkins1_651_3/run.sh
@@ -13,20 +13,27 @@ if [ $# -lt 0 ] || [ $# -gt 1 ]; then
 fi
 
 JENKINS_PORT=${1:-8089}
-JENKINS_HOME=/jenkins
-JENKINS_BIN_DIR=${JENKINS_HOME}/bin
+JENKINS_ROOT=/jenkins
+JENKINS_HOME=${JENKINS_ROOT}/data
+JENKINS_BIN_DIR=${JENKINS_ROOT}/bin
 JENKINS_WAR_PATH=${JENKINS_BIN_DIR}/jenkins.war
-JENKINS_LOG_DIR=${JENKINS_HOME}/log
-JENKINS_OUT_LOG_PATH=${JENKINS_LOG_DIR}/out.log
+JENKINS_LOG_DIR=${JENKINS_ROOT}/log
+JENKINS_ACCESS_LOG_FILE=access.log
+JENKINS_ACCESS_LOG_PATH=${JENKINS_LOG_DIR}/${JENKINS_ACCESS_LOG_FILE}
 
 HOST_JENKINS_PORT=${JENKINS_PORT}
+HOST_JENKINS_HOME=${SCRIPT_DIR}/data
 HOST_JENKINS_LOG_DIR=${SCRIPT_DIR}/log
+mkdir -p ${HOST_JENKINS_HOME}
+mkdir -p ${HOST_JENKINS_LOG_DIR}
+sudo rm -f ${HOST_JENKINS_LOG_DIR}/*
 
 docker run -itd \
   --name jenkins_1.651.3 \
   --hostname jenkins_1.651.3 \
   -p ${HOST_JENKINS_PORT}:${JENKINS_PORT} \
   -v ${HOST_JENKINS_LOG_DIR}:${JENKINS_LOG_DIR} \
+  -v ${HOST_JENKINS_HOME}:${JENKINS_HOME} \
   -w ${JENKINS_HOME} \
   centos:7_jenkins_1.651.3 \
-  bash -c "java -jar ${JENKINS_WAR_PATH} --httpPort=${JENKINS_PORT} --logfile=${JENKINS_OUT_LOG_PATH}"
+  bash -c "java -jar ${JENKINS_WAR_PATH} --httpPort=${JENKINS_PORT} --accessLoggerClassName=winstone.accesslog.SimpleAccessLogger --simpleAccessLogger.format=common --simpleAccessLogger.file=${JENKINS_ACCESS_LOG_PATH}"
