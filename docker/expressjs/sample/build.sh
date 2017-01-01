9482fb1523aae073819f75d59d2d2f6ec67db7ea
Lhacker <starclown1@gmail.com>
2017-01-02 00:34:43 +0900

    update express installation flow

--- docker/expressjs/sample/build.sh
+++ docker/expressjs/sample/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-EXPRESS_VERSION=latest
-docker build --force-rm --no-cache --build-arg EXPRESS_VERSION=${EXPRESS_VERSION} --build-arg EXPRESS_ROOT=/express_root -t centos:7_expressjs_${EXPRESS_VERSION} ${SCRIPT_DIR}
+EXPRESS_VERSION=4.14.0
+docker build --force-rm --no-cache
+  --build-arg EXPRESS_VERSION=${EXPRESS_VERSION} \
+  --build-arg EXPRESS_VIEW_OPTION=vash \
+  --build-arg EXPRESS_ROOT=/express_root \
+  --build-arg EXPRESS_APP_NAME=myapp \
+  -t centos:7_expressjs_${EXPRESS_VERSION} ${SCRIPT_DIR}
