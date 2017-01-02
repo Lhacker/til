1cdc9d0b79c3a72cf328c4b3d5d9bcf418377ec9
Lhacker <starclown1@gmail.com>
2017-01-02 22:56:44 +0900

    fix bug and make it work

--- docker/expressjs/sample/build.sh
+++ docker/expressjs/sample/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 EXPRESS_VERSION=4.14.0
-docker build --force-rm --no-cache
+docker build --force-rm --no-cache \
   --build-arg EXPRESS_VERSION=${EXPRESS_VERSION} \
   --build-arg EXPRESS_VIEW_OPTION=vash \
   --build-arg EXPRESS_ROOT=/express_root \
--- docker/expressjs/sample/run_server.sh
+++ docker/expressjs/sample/run_server.sh
@@ -4,17 +4,25 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 function usage() {
   echo 'Usage:'
-  echo "  $0"
+  echo "  $0 [express version=4.14.0]"
   exit 1
 }
 
+EXPRESS_VERSION=${1:-4.14.0}
+EXPRESS_APP_NAME=myapp
 EXPRESS_ROOT=/express_root
+EXPRESS_APP_DIR=${EXPRESS_ROOT}/${EXPRESS_APP_NAME}
+
+HOST_EXPRESS_APP_DIR=${SCRIPT_DIR}/${EXPRESS_APP_NAME}
 
 docker run -itd \
   --name expressjs_server \
   --hostname expressjs_server \
-  -v "${HOST_EXPRESS_ROUTES_DIR}:${EXPRESS_ROUTES_DIR}" \
+  -v "${HOST_EXPRESS_APP_DIR}/app.js:${EXPRESS_APP_DIR}/app.js" \
+  -v "${HOST_EXPRESS_APP_DIR}/bin:${EXPRESS_APP_DIR}/bin" \
+  -v "${HOST_EXPRESS_APP_DIR}/public:${EXPRESS_APP_DIR}/public" \
+  -v "${HOST_EXPRESS_APP_DIR}/routes:${EXPRESS_APP_DIR}/routes" \
+  -v "${HOST_EXPRESS_APP_DIR}/views:${EXPRESS_APP_DIR}/views" \
   -p "80:80" \
   -p "443:443" \
-  centos:7_expressjs_latest \
-  npm start
+  centos:7_expressjs_${EXPRESS_VERSION}
