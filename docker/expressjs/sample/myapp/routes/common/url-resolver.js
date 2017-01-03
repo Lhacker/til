b086416a8472eba9807213ee21b2998a42a24311
Lhacker <starclown1@gmail.com>
2017-01-03 18:39:58 +0900

    removed(changed routes structure)

--- docker/expressjs/sample/myapp/routes/common/url-resolver.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-module.exports = function(dirName, fileName) {
-  var pathWithoutExtension = path.join(dirName, path.basename(fileName, '.js'));
-  var delimitedPath = pathWithoutExtension.split(path.sep);
-  return '/' + delimitedPath.slice(delimitedPath.indexOf('routes') + 1).join('/');
-};
