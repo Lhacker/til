561b50d44af637651bdc5411ecc642c388d38b4c
Lhacker <starclown1@gmail.com>
2017-01-07 19:33:25 +0900

    modify docker image

--- docker/rundeck/sample/build.sh
+++ docker/rundeck/sample/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-HOST_RUNDECK_PORT=${1:-4440}
 
-docker run -d --privileged --name rundeck_test --port 4440:4440 centos:7_rundeck /sbin/init
+docker build --force-rm --no-cache -t centos7-rundeck:dev ${SCRIPT_DIR}
--- docker/rundeck/sample/run.sh
+++ docker/rundeck/sample/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 HOST_RUNDECK_PORT=${1:-4440}
 
 docker run -d --privileged \
-  --name rundeck_test \
+  --name rundeck-dev \
   --port ${HOST_RUNDECK_PORT}:4440 \
-  centos:7_rundeck \
+  centos7-rundeck:dev \
   /sbin/init
