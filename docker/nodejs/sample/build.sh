fe601243dd8a82df4fa4de82968e729d585c8ec3
Lhacker <starclown1@gmail.com>
2017-01-07 18:02:12 +0900

    modify bit

--- docker/nodejs/sample/build.sh
+++ docker/nodejs/sample/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-docker build --force-rm --no-cache --build-arg NODEJS_VERSION=v7.0.0 -t centos:7_nodejs_v7.0.0 $SCRIPT_DIR
+NODEJS_VERSION=${1:-v7.0.0}
+docker build --force-rm --no-cache --build-arg NODEJS_VERSION=${NODEJS_VERSION} -t centos7-nodejs:${NODEJS_VERSION} ${SCRIPT_DIR}
