539c6e56fb3857b2bbbd6e26b4d37bef8672c84d
Lhacker <starclown1@gmail.com>
2017-04-25 00:33:11 +0900

    update redis default version

--- docker/redis/sample/build.sh
+++ docker/redis/sample/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-REDIS_VERSION=${1:-3.2.5}
+REDIS_VERSION=${1:-3.2.6}
 
 docker build --force-rm --no-cache --build-arg REDIS_VERSION=${REDIS_VERSION} -t centos7-redis:${REDIS_VERSION} ${SCRIPT_DIR}
