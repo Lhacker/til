c641301b903bd0cb7c18d6639925ca6513f2b2a9
Lhacker <starclown1@gmail.com>
2017-06-16 21:02:56 +0900

    update so that can connect to running container

--- docker/rcxxxxi/bin/migrate_db.sh
+++ docker/rcxxxxi/bin/migrate_db.sh
@@ -2,8 +2,13 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-cd ${SCRIPT_DIR}/.. && docker-compose run webapp bash -c " \
-  source ~/.bashrc && \
+if [ "$(docker ps -a | grep -E 'rcxxxxi-webapp')" = "" ] && [ "$(docker ps -a | grep 'rcxxxxi-database')" = "" ]; then
+  echo Please up webapp and database container >&2
+  exit
+fi
+
+docker exec -it rcxxxxi-webapp bash -c " \
+  source /etc/profile && \
   php artisan migrate && \
   :"
 
