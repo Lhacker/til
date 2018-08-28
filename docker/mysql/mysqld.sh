3a14e2844ea88d57071d27ed1c3d46f9949f8ddd
Lhacker <starclown1@gmail.com>
2018-08-28 12:20:34 +0900

    fix:

--- docker/mysql/mysqld.sh
+++ docker/mysql/mysqld.sh
@@ -7,8 +7,9 @@ function input_password() {
   echo 1>&2
 }
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 MYSQL_VERSION=${1:-5.7}
 MYSQL_ROOT_PASSWORD=$(input_password)
 NAME=mysqld
 
-docker run -d --name ${NAME} -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -p 3306:3306 mysql:5.7
+docker run -d --name ${NAME} -v ${SCRIPT_DIR}/mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -p 3306:3306 mysql:5.7
